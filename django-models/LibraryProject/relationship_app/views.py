2ebfcd441740664f8ad5446b56c3e7da4d07f7ce
Senelor K. Sebuava <123073561+SSenelor@users.noreply.github.com>
2025-07-20 17:14:35 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -140,3 +140,11 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
+from django.shortcuts import render, get_object_or_404
+from .models import Library
+
+def library_detail(request, library_id):
+    library = get_object_or_404(Library, id=library_id)
+    return render(request, "relationship_app/library_detail.html", {"library": library})
+
