14a7d4948182235184bbe8b1bc00d0bb1b10d11e
Senelor K. Sebuava <123073561+SSenelor@users.noreply.github.com>
2025-08-02 13:55:12 +0000

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,29 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Columns shown in list view
     list_filter = ('publication_year', 'author')            # Sidebar filter options
     search_fields = ('title', 'author')                     # Search bar for title and author
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser  # if your CustomUser is now in bookshelf.models
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+        }),
+    )
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
